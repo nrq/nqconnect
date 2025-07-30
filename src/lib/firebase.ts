90cb2b18df5e909aa2c877b3129eff0a6aea9d6a
fire base <firebase@nrquk.com>
2025-07-30 21:56:05 +0000

    same error - config not found, can you reconnect the app to the right fi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyCpCyqW7oS5tZsxYdVccy6MUE_JNuWeCbs",
   authDomain: "salamconnect-2rlmv.firebaseapp.com",
   projectId: "salamconnect-2rlmv",
-  storageBucket: "salamconnect-2rlmv.firebasestorage.app",
+  storageBucket: "salamconnect-2rlmv.appspot.com",
   messagingSenderId: "286074217686",
   appId: "1:286074217686:web:fecea9d16c06cc4814d577",
 };
 
 // Initialize Firebase
+// Check if apps are already initialized to prevent errors.
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const storage = getStorage(app);
