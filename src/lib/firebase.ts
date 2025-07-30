9f58c71407e8ac286c10cbc2a575a223da3c246f
fire base <firebase@nrquk.com>
2025-07-30 21:50:21 +0000

    same message

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCpCyqW7oS5tZsxYdVccy6MUE_JNuWeCbs",
+  authDomain: "salamconnect-2rlmv.firebaseapp.com",
+  projectId: "salamconnect-2rlmv",
+  storageBucket: "salamconnect-2rlmv.firebasestorage.app",
+  messagingSenderId: "286074217686",
+  appId: "1:286074217686:web:fecea9d16c06cc4814d577",
 };
 
 // Initialize Firebase
