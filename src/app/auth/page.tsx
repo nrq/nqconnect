e163b8ba93f62baefbc562a8c0ec187e0a68c25f
fire base <firebase@nrquk.com>
2025-07-31 02:38:52 +0000

    {   "logName": "projects/salamconnect-2rlmv/logs/cloudaudit.googleapis.c

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-
 import { AuthForm } from "@/components/auth/auth-form";
 
-export default function AuthPage() {
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-background p-4">
-            <div className="w-full max-w-md">
-                <AuthForm />
-            </div>
-        </div>
-    );
-}
+export default AuthForm;
